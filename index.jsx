7bc7cb9f3029030738ef82b2f90839c5941b7856
Emilio de la Torre <emilio.delatorre@ft.com>
2022-09-27 17:04:44 +0100

    Add markup and styles
    Install origami dependencies
    Adds markup with temporary hardcoded values
    Adds styles

--- index.jsx
+++ index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 export function BrowsableLists () {
+
 	return (
-		<div></div>
+		<div className='browsable-lists'>
+			<h2 className='browsable-lists-heading'>Curated list by a university professor</h2>
+			<div className='browsable-lists-title'>
+				<h3 className='browsable-lists-title-heading'>Politics in North America</h3>
+				<p className='browsable-lists-title-text'>18 Saved articles</p>
+			</div>
+			<ul className='browsable-lists-list'>
+				<li className='browsable-lists-list-item'><a href="#">The everything rally: vaccines prompt wave of market exuberance</a></li>
+				<li className='browsable-lists-list-item'><a href="#">The new social contract and your money</a></li>
+				<li className='browsable-lists-list-item'><a href="#">EU capitals urge Barnier to take tougher line with UK</a></li>
+				<li className='browsable-lists-list-item'><a href="#">The everything rally: vaccines prompt wave of market exuberance</a></li>
+			</ul>
+		</div>
 	)
 }
