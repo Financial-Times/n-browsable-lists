ee53b0d7a9959f63743660cdb006e788a2599aef
Emilio de la Torre <emilio.delatorre@ft.com>
2022-10-11 16:33:35 +0100

    Set up tracking on demo page

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,6 +1,14 @@
+const oTracking = require('@financial-times/o-tracking').default;
 const { init: browsableListsInit } = require('../../main.js');
 
 document.documentElement.classList.add('js', 'enhanced');
+
+oTracking.init({
+	context: { product: 'next' },
+	test: true
+});
+
+oTracking.click.init('cta');
 
 browsableListsInit({
 	parentSelector: '.rhr'
