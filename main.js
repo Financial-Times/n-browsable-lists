37ee967f9a44fa064fb14150a38013364a4bb320
Emilio de la Torre <emilio.delatorre@ft.com>
2022-10-11 12:26:36 +0100

    Added logic to fetch data for publicList
    Adds the main.js file that contains the client-side logic for this component
    Reads the concepts list from the SSR markup, gets the first concept and matches it against the conceptListMapping to get a listId
    Fetches the data from myFTClient using the new endpoint getPublicList. However, the endpoint is not ready at the time of this commit.
    So, it temporarily adds a fallback with a hardcoded list data
    Finally, it hydrates the SSR markup with the listData

---
+++ main.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as dataEmbed from '@financial-times/dotcom-ui-data-embed';
+import myftClient from 'next-myft-client';
+
+import { conceptListMap } from './concept-list-map';
+import { BrowsableLists } from '.';
+
+export async function init({ parentSelector }) {
+	const dataEmbedClient = dataEmbed.init({ id: 'browsable-lists-data' });
+
+	const concepts = dataEmbedClient.get('concepts');
+	const container = document.querySelector(parentSelector);
+
+	if (container && concepts) {
+		const matchedList =
+			Array.isArray(concepts) &&
+			concepts.length &&
+			conceptListMap.find((pair) => pair.conceptId === concepts[0]);
+
+		const listId = matchedList?.listId;
+
+		const listData = await myftClient
+			.init()
+			.then(() => myftClient.getPublicList(listId))
+			.catch(() =>
+				// Temporarily returning this object while the getPublicList method is not ready
+				({
+					uuid: listId,
+					name: 'Climate Change Reading List',
+					items: [
+						{
+							uuid: 'e4de7a58-1128-42c3-9989-d302d913f2c8',
+							title:
+								'New York climate week: Carney concedes banks shrinking from green guidelines'
+						},
+						{
+							uuid: '25a0f06a-b939-4a39-badb-103bf9a38361',
+							title: 'Why Al Gore is feeling upbeat as COP27 approaches'
+						},
+						{
+							uuid: '31061741-16a1-49fe-a495-6d7dbd3c886a',
+							title:
+								'Flood-hit Pakistan should suspend debt repayments, says UN policy paper'
+						}
+					]
+				})
+			);
+
+		ReactDOM.render(
+			<BrowsableLists listData={listData} concepts={concepts} />,
+			container
+		);
+	}
+}
